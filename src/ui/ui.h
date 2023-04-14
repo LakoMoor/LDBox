c60fd54a268cbd7a27a2f5e9aa1c9eac0f216b55
lakomoor <lakomoor@gmail.com>
2023-04-14 14:16:34 +0300

    Fix bugs, added url, make json file, requests to github API

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -19,7 +19,23 @@ public:
     static void DrawImGui();
     static void DestroyImGui();
 
+
+    //tools
+    static void openurl(const char* path)
+    {
+        #ifdef _WIN32
+            ::ShellExecuteA(NULL, "open", path, NULL, NULL, SW_SHOWDEFAULT);
+        #else
+        #if __APPLE__
+            const char* open_executable = "open";
+        #else
+            const char* open_executable = "xdg-open";
+        #endif
+            char command[256];
+            snprintf(command, 256, "%s \"%s\"", open_executable, path);
+            system(command);
+        #endif
+    }
 private:
-     
     int test;
 };
