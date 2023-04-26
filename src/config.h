bdecee1839258a3b12e5d562200c6a9f7a614223
lakomoor <lakomoor@gmail.com>
2023-04-26 14:39:25 +0300

    config header

---
+++ src/config.h
@@ -0,0 +1,10 @@
+bool show_launcher = true;
+bool headerui = false;
+bool debugui = false;
+static float R = 0.2f;
+static float G = 0.3f;
+static float B = 0.3f;
+
+
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
