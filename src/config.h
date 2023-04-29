a886d626cbd0da442ad3cc79509d42e8fe011eb0
unknown <lakomoor@gmail.com>
2023-04-29 21:10:41 +0300

    Some add

--- src/config.h
+++ src/config.h
@@ -1,10 +1,12 @@
-bool show_launcher = true;
+static bool show_launcher;
+static bool show_launchermobile;
 bool headerui = false;
 bool debugui = false;
 static float R = 0.2f;
 static float G = 0.3f;
 static float B = 0.3f;
-
+static int GET_WIDTH;
+static int GET_HEIGHT;
 
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
